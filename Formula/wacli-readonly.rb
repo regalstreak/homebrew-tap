e09f013bb5a683c3dd0a4406866a245454bf0454
Neil Agarwal <neil@neilagarwal.me>
2026-02-01 23:26:17 +0530

    Update to v0.2.2 and fix formula location
    - Update wacli-readonly to v0.2.2
    - Remove misplaced formula from root (should be in Formula/)

--- Formula/wacli-readonly.rb
+++ Formula/wacli-readonly.rb
@@ -1,22 +1,22 @@
 class WacliReadonly < Formula
   desc "Read-only WhatsApp CLI - fork of wacli with send capabilities removed"
   homepage "https://github.com/regalstreak/wacli-readonly"
-  version "0.2.0"
+  version "0.2.2"
   license "MIT"
 
   on_macos do
     url "https://github.com/regalstreak/wacli-readonly/releases/download/v#{version}/wacli-readonly-macos-universal.tar.gz"
-    sha256 "0893fa12db5c6125b0d6a0963aa603ec7968bd77e16bf907ad4ca5ed4d9e2b45"
+    sha256 "6417c6edcce6b52ada7c2ce3b4e74bbcaf3c5cc817acd17b66582eccf9a3a17d"
   end
 
   on_linux do
     on_intel do
       url "https://github.com/regalstreak/wacli-readonly/releases/download/v#{version}/wacli-readonly-linux-amd64.tar.gz"
-      sha256 "c84983ad1dc834be196802cb396c934b0fd7c34ec521408e22eb79fc86ef14da"
+      sha256 "d3ac5272a829179dfd32cc410d0010cebd724abe594779d14a4b8afb2554e5fa"
     end
     on_arm do
       url "https://github.com/regalstreak/wacli-readonly/releases/download/v#{version}/wacli-readonly-linux-arm64.tar.gz"
-      sha256 "74105590591a9660368ea48dd7c0add2bed65ca6dbb09998fb2141947b518294"
+      sha256 "ee750ac956d0c944dd9c94f213ee547fbed4d734c5448bbb64fbf4fd2112ca80"
     end
   end
 
